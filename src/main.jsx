f274162b0b44204b19a6d0ec87ff2bd8c160cdca
mrbubu0112 <hoangminhtuan0112@gmail.com>
2024-04-06 08:43:33 +0700

    update code

--- src/main.jsx
+++ src/main.jsx
@@ -4,10 +4,12 @@ import App from './App.jsx'
 import './index.css'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import 'bootstrap/dist/js/bootstrap.bundle.min'
+import CssBaseline from '@mui/material/CssBaseline';
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
+    <CssBaseline />
     <App />
   </React.StrictMode>
 )
